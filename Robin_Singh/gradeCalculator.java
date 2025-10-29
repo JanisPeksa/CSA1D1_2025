d8d5a934e0c510aa419fff29855e4eeb3ed32396
Robin <robin.webdev.me@gmail.com>
2025-10-29 17:37:08 +0200

    Add grade calculator functionality
    Implement grade calculator that evaluates and displays letter grades based on user input.

---
+++ Robin_Singh/gradeCalculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class New {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter you grade between 0 to 100 : ");
+        int grade = scan.nextInt();
+
+        if (grade < 0 && grade > 100){
+            System.out.println("Invalid Score. Please enter a value between 0 and 100");
+        } else{
+            if (grade >= 90){
+                System.out.println("Your grade is : A");
+                System.out.println("Excellent Work");
+            } else if (grade >= 80){
+                System.out.println("Your grade is : B");
+            } else if (grade >= 70){
+                System.out.println("Your grade is : C");
+            } else if (grade >= 60){
+                System.out.println("Your grade is : D");
+            } else{
+                System.out.println("Your grade is : F");
+            }
+        }
+
+
+
+    }
+}
