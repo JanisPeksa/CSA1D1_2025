9c9e4b7a876a609f183732c419a6de38dae92fc3
Nurmuhammad Nazirov <nnazirov567@gmail.com>
2025-11-18 21:46:09 +0200

    Add files via upload

---
+++ Nurmuhammad_Nazirov/Homeworks/banana.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class banana {
+
+    public static void main(String[] args) {
+
+        System.out.println("BBBB");
+        System.out.println("B   B");
+        System.out.println("BBBB");
+        System.out.println("B   B");
+        System.out.println("BBBB");
+        System.out.println("    ");
+        System.out.println(" AAA ");
+        System.out.println("A   A");
+        System.out.println("AAAAA");
+        System.out.println("A   A");
+        System.out.println("A   A");
+        System.out.println("     ");
+        System.out.println("N   N");
+        System.out.println("NN  N");
+        System.out.println("N N N");
+        System.out.println("N   N  ");
+        System.out.println("    ");
+        System.out.println(" AAA ");
+        System.out.println("A   A");
+        System.out.println("AAAAA");
+        System.out.println("A   A");
+        System.out.println("A   A");
+        System.out.println("     ");
+        System.out.println("N   N");
+        System.out.println("NN  N");
+        System.out.println("N N N");
+        System.out.println("N   N  ");
+        System.out.println("    ");
+        System.out.println(" AAA ");
+        System.out.println("A   A");
+        System.out.println("AAAAA");
+        System.out.println("A   A");
+        System.out.println("A   A");
+
+    }
+
+
+
+}
