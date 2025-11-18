3f5286045594df8e0e5118b37b7ea7d6504458f0
Nurmuhammad Nazirov <nnazirov567@gmail.com>
2025-11-19 00:01:51 +0200

    Real temperature.java
    Firstly i add wrong cod

--- Nurmuhammad_Nazirov/Homeworks/temperature.java
+++ Nurmuhammad_Nazirov/Homeworks/temperature.java
@@ -1,29 +1,21 @@
- void main() {
+import java.util.Scanner;
 
-    Scanner sc = new Scanner(System.in);
-    IO.print("Enter your temperature in Celsius: ");
-    int temp = sc.nextInt();
-    if (0 > temp) {
-        IO.println("it's freazing");
-    } else if (0 <= temp && temp < 15) {
-        IO.println("it's cold");
-    } else if (15 <= temp && temp < 25) {
-        IO.println("it's warm");
-    } else if (25 <= temp) {
-        IO.println("it's hot");
-    } else if (temp >= 70) {
-        IO.println(("C"));
-    } else if (temp >= 60) {
-        IO.println(("D"));
-    } else {
-        IO.println(('F'));
+public class temperature {
+    static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        IO.print("Enter your temperature in Celsius: ");
+        int temp = sc.nextInt();
+        if (0 > temp) {
+            IO.println("it's freazing");
+        } else if (temp < 15) {
+            IO.println("it's cold");
+        } else if (temp < 25) {
+            IO.println("it's warm");
+        } else {
+            IO.println("it's hot");
+        }
 
     }
-
 }
 
-void main() {
-}
-
-void main() {
-}
