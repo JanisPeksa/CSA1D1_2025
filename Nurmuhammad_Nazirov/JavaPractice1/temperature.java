26a93e4cb9a47de3128f5a477a2713dc6a53834f
Nurmuhammad Nazirov <nnazirov567@gmail.com>
2025-11-19 02:02:58 +0200

    Create temperature.java
    replace to the correct folder

---
+++ Nurmuhammad_Nazirov/JavaPractice1/temperature.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class temperature {
+    static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        IO.print("Enter your temperature in Celsius: ");
+        int temp = sc.nextInt();
+        if (0 > temp) {
+            IO.println("it's freazing");
+        } else if (temp < 15) {
+            IO.println("it's cold");
+        } else if (temp < 25) {
+            IO.println("it's warm");
+        } else {
+            IO.println("it's hot");
+        }
+
+    }
+}
