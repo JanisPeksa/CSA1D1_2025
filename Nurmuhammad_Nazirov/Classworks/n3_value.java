4059494353e74671516503aebedc1eee8f24feb2
Nurmuhammad Nazirov <nnazirov567@gmail.com>
2025-12-01 03:55:36 +0200

    Create n3_value.java

---
+++ Nurmuhammad_Nazirov/Classworks/n3_value.java
@@ -0,0 +1,12 @@
+public class n3_value {
+    public static void main(String[] args) {
+        int x = 5; 
+        System.out.println(x);
+        System.out.println("x = x + x + 2 => x = "+ x +" + "+ x +" + 2");
+        x = x + x + 2;
+        System.out.println(x);
+        System.out.println("x = x + x + 1 => x = "+ x +" + "+ x +" + 1");
+        x = x + x + 1; 
+        System.out.println(x);
+        }
+}
