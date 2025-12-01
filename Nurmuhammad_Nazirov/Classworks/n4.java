e9e35cc111be42f2c74ae72c5e1dfac836717355
Nurmuhammad Nazirov <nnazirov567@gmail.com>
2025-12-01 03:59:52 +0200

    Create n4.java

---
+++ Nurmuhammad_Nazirov/Classworks/n4.java
@@ -0,0 +1,13 @@
+public class n4 {
+    public static void main(String[] args) {
+                int x = 15;
+                int y = 100;
+                System.out.println(x>y);
+                System.out.println(x < 15); 
+                System.out.println(x <= 15);
+                System.out.println(x == y);
+                System.out.println(x != 5);
+                System.out.println(x * -y >0 ); 
+                boolean isBigger = x > y;
+            }
+        }
