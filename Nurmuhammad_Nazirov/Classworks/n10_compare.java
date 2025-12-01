d99448a3a51c485325b2fb18b3ffc5ea0c972af4
Nurmuhammad Nazirov <nnazirov567@gmail.com>
2025-12-01 04:07:07 +0200

    Create n10_compare.java

---
+++ Nurmuhammad_Nazirov/Classworks/n10_compare.java
@@ -0,0 +1,15 @@
+public class n10_compare {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+        if (num1 == num2) {
+            System.out.println(" Both are equal ");
+        }
+        else if(num1 > num2) {
+            System.out.println("First is larger");
+        }
+        else if(num1 < num2) {
+            System.out.println("Second is larger");
+        }
+    }
+}
